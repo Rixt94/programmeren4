863ac3721c1154efee7dec6b9f2f167c81acbf3b
Rixt94 <rixt@denhoedt.nl>
2018-04-19 15:51:54 +0200

    gewijzigde poort

--- serverJS.js
+++ serverJS.js
@@ -2,6 +2,8 @@
 
 let http = require('http');
 
+const port = process.env.PORT || 3000;
+
 function onRequest(request, resonse) {
     console.log("Er was een request");
     resonse.writeHead(200, {'Content-Type': 'application/json'});
@@ -13,5 +15,5 @@ function onRequest(request, resonse) {
     resonse.end(result);
 }
 
-http.createServer(onRequest).listen(3000);
-console.log("De server luistert op poort 3000");
+http.createServer(onRequest).listen(port);
+console.log("De server luistert op poort " + port);

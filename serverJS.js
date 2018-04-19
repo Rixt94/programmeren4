d40efccbec35d79c520155836ae65688dc5dfe2f
Rixt94 <rixt@denhoedt.nl>
2018-04-19 15:27:43 +0200

    Intial commit

---
+++ serverJS.js
@@ -0,0 +1,15 @@
+let http = require('http');
+
+function onRequest(request, resonse) {
+    console.log("Er was een request");
+    resonse.writeHead(200, {'Content-Type': 'application/json'});
+    let result = JSON.stringify({
+        'fname': 'fjsjkk',
+        'flast': 'jdhai'
+    });
+    //resonse.write(JSON.stringify(result));
+    resonse.end(result);
+}
+
+http.createServer(onRequest).listen(3000);
+console.log("De server luistert op poort 3000");
